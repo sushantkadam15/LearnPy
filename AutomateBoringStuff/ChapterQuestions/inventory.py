d1ab9bd3d6b92b592b89a6fd3ccf7d046f7301a5
sushant <sushantkadam15@gmail.com>
2020-03-30 13:19:33 -0400

    update

---
+++ AutomateBoringStuff/ChapterQuestions/inventory.py
@@ -0,0 +1,10 @@
+openFile = open("inventorylist.txt")
+print(openFile.read())
+
+"""def inventoryListAdd():
+    key = input("Please input the Item you would like to add: ")
+    print("Please input the number of", key, "you need: ")
+    value = int(input("> "))
+    inventoryList.update({key : value})
+
+inventoryListAdd()"""
